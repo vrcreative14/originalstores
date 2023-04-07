6b252ec9949f1c212a6c2d2108089c12f428b645
vrcreative14 <69114615+vrcreative14@users.noreply.github.com>
2023-04-07 11:10:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 poetry install
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations
+python3 manage.py migrate
