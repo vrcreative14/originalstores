a2485b55d68b0b677c8cd599383ba750560882b1
vrcreative14 <69114615+vrcreative14@users.noreply.github.com>
2024-03-06 13:14:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 pip install -r requirements.txt
+pip install gunicorn uvicorn
+pip freeze > requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate
